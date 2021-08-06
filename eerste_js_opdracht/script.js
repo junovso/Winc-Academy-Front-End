3c40dc88708528963be1a5f43f814d1cd4fbaf74
junovso <67838043+junovso@users.noreply.github.com>
2021-08-06 13:44:20 +0200

    Opdracht: Eerste javascript opdracht

---
+++ eerste_js_opdracht/script.js
@@ -0,0 +1,15 @@
+let naam = "juno";
+//let if = true; <-- dit werkt niet omdat if een woord is dat in javascript al gereserveerd is ( If/Else statements)
+let nummer1 = 4;
+let nummer2 = 5;
+let strnummer1 = "4";
+let strnummer2 = "5";
+let keersom = 5 * 20; // dit werkt ook.
+let leeftijd = 20;
+let strleeftijd = "20";
+console.log(naam);
+console.log(nummer1 + nummer2); // hier telt js ze op omdat we ze als nummer geschreven hebben . (zonder de haakjes)
+console.log(strnummer1 + strnummer2); // hier telt js niet bij elkaar op maar voegt hij de getallen samen omdat ze in een string staan.
+console.log(keersom);
+console.log(typeof leeftijd);
+console.log(typeof strleeftijd);
