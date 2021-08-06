ce04a5bf255965482d3bb09a3c584b8066909de4
junovso <67838043+junovso@users.noreply.github.com>
2021-08-06 14:38:28 +0200

    Opdracht: JS If/Else

---
+++ js_if_else/script.js
@@ -0,0 +1,21 @@
+const age = 18;
+const isFemale = true;
+const driverStatus = "bob";
+
+if (age >= 18) {
+  document.write("Ouder dan 18");
+} else {
+  document.write("Helaas! je mag niet naar binnen.");
+}
+
+if (isFemale) {
+  document.write("Jij bent een vrouw");
+} else {
+  document.write("Jij bent een man");
+}
+
+if (driverStatus == "bob") {
+  document.write("Jij hebt niet gedronken en mag rijden!");
+} else {
+  document.write("Helaas, jij hebt gedronken!");
+}
