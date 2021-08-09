54c3d4061100d83e89685d07dbdef23ba5c0d63e
junovso <67838043+junovso@users.noreply.github.com>
2021-08-09 14:17:36 +0200

    Javascript functions exercise

---
+++ js_functions_exercise/script.js
@@ -0,0 +1,21 @@
+// function makePizza
+const makePizza = function () {
+  console.log("kneed deeg");
+  console.log("Beleg het deeg");
+  console.log("Plaats de pizza in de oven");
+  console.log("eet de pizza op");
+};
+
+const makeSushi = function () {
+  console.log("Pak wat zeewier");
+  console.log("Beleg met rijst en rol op");
+  console.log("Voeg een stukje zalm toe aan de bovenkant");
+  console.log("eet de sushi op");
+};
+
+// Call function 3 times
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
