041559a0b8a1c8fa3711108870f47dbf45528bfc
junovso <67838043+junovso@users.noreply.github.com>
2021-08-10 11:40:39 +0200

    update js functions ex4

--- js_functions_exercise_4/script.js
+++ js_functions_exercise_4/script.js
@@ -1,8 +1,9 @@
 function square1(number1, number2) {
   nummer1 = number1 * number1;
   nummer2 = number2 * number2;
-  number3 = nummer1 * nummer2;
-  return number3;
+  nummer3 = nummer1 + nummer2;
+  number4 = nummer3 * nummer3;
+  return number4;
 }
 
 console.log(square1(4, 5));
@@ -10,8 +11,9 @@ console.log(square1(4, 5));
 const square2 = function (number1, number2) {
   nummer1 = number1 * number1;
   nummer2 = number2 * number2;
-  number3 = nummer1 * nummer2;
-  return number3;
+  nummer3 = nummer1 + nummer2;
+  number4 = nummer3 * nummer3;
+  return number4;
 };
 
 console.log(square2(4, 5));
@@ -19,8 +21,9 @@ console.log(square2(4, 5));
 const square3 = (number1, number2) => {
   nummer1 = number1 * number1;
   nummer2 = number2 * number2;
-  number3 = nummer1 * nummer2;
-  return number3;
+  nummer3 = nummer1 + nummer2;
+  number4 = nummer3 * nummer3;
+  return number4;
 };
 
 console.log(square2(4, 5));
