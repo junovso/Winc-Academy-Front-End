6a54197ee5240c1adc4e28695d55195bc042ed13
junovso <67838043+junovso@users.noreply.github.com>
2021-08-10 14:03:49 +0200

    For & while loop exercise.

---
+++ exercise_while_for_loops/multiplication-tables.js
@@ -0,0 +1,6 @@
+for (let i = 0; i <= 10; i++) {
+  // console.log(i + " keer 9 = " + i * 9); // dit gebruiken we alleen zonder de nested for loop
+  for (let b = 0; b <= 10; b++) {
+    console.log(i + " keer " + b + " = " + i * b);
+  }
+}
