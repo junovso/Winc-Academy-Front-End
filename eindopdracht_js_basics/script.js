dd71a32681a21f742002f257530455aec6d31639
junovso <67838043+junovso@users.noreply.github.com>
2021-08-16 11:25:46 +0200

    Update script.js

--- eindopdracht_js_basics/script.js
+++ eindopdracht_js_basics/script.js
@@ -5,7 +5,7 @@ var i;
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function () {
     this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
+    let dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
     } else {
