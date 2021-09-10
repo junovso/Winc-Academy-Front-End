37a5715e7f63be9181c8e96bf948bb0576d0899f
junovso <67838043+junovso@users.noreply.github.com>
2021-09-10 12:12:54 +0200

    opdracht: jest testing

---
+++ jest-testing/script.js
@@ -0,0 +1,17 @@
+const functions = require("./functions.js");
+
+test("Should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+test("checkValue Should be falsy when argument is undefined", () => {});
+
+test("User should be Brad Traversy object", () => {});
+test("Should be under or equal to 1600", () => {
+  const load1 = 800;
+  const load2 = 800;
+});
+test("There is no I in team", () => {});
+test("Admin should be in usernames", () => {
+  usernames = ["john", "karen", "admin"];
+});
